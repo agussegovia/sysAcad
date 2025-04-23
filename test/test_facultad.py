5a2e6f8af5f7a0f9bb1ddf3d04e806f3bcc84b98
agussegovia <anico.segovia@gmail.com>
2025-04-23 15:36:17 -0300

    Actualizo el archivo test_facultad

--- test/test_facultad.py
+++ test/test_facultad.py
@@ -1,19 +1,5 @@
 from dataclasses import dataclass 
 
-class Facultad():
-    def __init(self):
-        self.nombre = None
-        self.abreviatura = None
-        self.directorio = None
-        self.sigla = None
-        self.codigoPostal = None
-        self.ciudad = None
-        self.domicilio = None
-        self.telefono = None
-        self.contacto = None
-        self.email = None
-
-
 @dataclass(init=False, repr=False, eq=False)
 class Facultad1(): 
     nombre: str 
